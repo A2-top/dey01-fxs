0b1a47ef9bd2b0f60334c857b89a6b8bf56276c6
wu-ying-qi <1467652859@qq.com>
2021-04-29 15:44:43 +0800

    项目初始化

---
+++ beta/src/components/index/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+      <img src="/imgs/logo.png" alt="" class="logo" />
+      <p>
+        <span>
+          <van-icon name="contact" size="2rem" />
+          <p>我的</p>
+        </span>
+        <span>
+          <van-icon name="wap-nav" size="2rem" />
+          <p>导航</p>
+        </span>
+      </p>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+header {
+  display: flex;
+  justify-content: space-between;
+  background: #fff;
+}
+header > p {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 6.25rem;
+}
+.logo {
+  width: 10rem;
+  margin: 0.375rem 0 0.625rem 0.625rem;
+}
+</style>
---
+++ beta/src/views/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+      <Header />
+  </div>
+</template>
+
+<script>
+import Header from '../components/index/Header';
+export default {
+    components:{
+        Header,
+    }
+}
+</script>
+
+<style>
+.index{
+    height: 100%;
+    width: 100%;
+    background: #eee;
+}
+</style>
